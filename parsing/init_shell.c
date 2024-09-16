cbd4b89bd50441d8fb7f00ac1a89480f50874cf7
bajjishaimae <bajjishaimae3@gmail.com>
2024-09-16 18:41:02 +0100

    read notes

--- parsing/init_shell.c
+++ parsing/init_shell.c
@@ -26,37 +26,38 @@ char **copy_env(char **env)
     return (to_copy);
 }
 
-/*int get_shlvl(char **env)
-{
-    int i;
-    int shlvl;
-    char *temp = NULL;
+// int get_shlvl(char **env)
+// {
+//     int i;
+//     int shlvl;
+//     char *temp = NULL;
     
-    i = 0;
-    while (env && env[i])
-    {
-        if (!strncmp(env[i], "SHLVL", 5))
-        {
-            temp = env[i];
-            break;
-        }
-        i++;
-    }
-    while (*temp != '=')
-        temp++;
-    temp++;
-    shlvl = ft_atoi(temp);
-}
-*/
-void set_shlvl(t_list shell)
-{
-    char *final;
-    char *nbr;
+//     i = 0;
+//     while (env && env[i])
+//     {
+//         if (!strncmp(env[i], "SHLVL", 5))
+//         {
+//             temp = env[i];
+//             break;
+//         }
+//         i++;
+//     }
+//     while (*temp != '=')
+//         temp++;
+//     temp++;
+//     shlvl = ft_atoi(temp);
+//     return (shlvl);
+// }
 
-    nbr = ft_itoa(shell.shlvl);
-    final = ft_strjoin("SHLVL=", nbr);
-    //need help from ayman to export it;// no thanks ask ali ; it's your idiot code not him ;)
-}
+// void set_shlvl(t_list shell)
+// {
+//     char *final;
+//     char *nbr;
+
+//     nbr = ft_itoa(shell.shlvl);
+//     final = ft_strjoin("SHLVL=", nbr);
+//     //need help from ayman to export it;// no thanks ask ali ; it's your idiot code not him ;)
+// }
 
 void display_prompt(t_list shell, char **env)
 {
