d2e158e3def90f23349bf123085d4617cdbdf818
Sebrani Aymane <sebraniayman@gmail.com>
2024-10-15 20:05:01 +0100

    hered

--- execution/heredoc.c
+++ execution/heredoc.c
@@ -6,7 +6,7 @@
 /*   By: asebrani <asebrani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/13 23:55:44 by asebrani          #+#    #+#             */
-/*   Updated: 2024/10/15 17:38:57 by asebrani         ###   ########.fr       */
+/*   Updated: 2024/10/15 19:55:58 by asebrani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,46 +17,60 @@
 #include <unistd.h>
 
 void handle_heredoc(t_line *final, env_vars *list, char **env) {
-  char *delim;
-  char *input;
-  char buffer[100];
-  char namefile[20];
-  int fd;
-  char last_input[10];
+	char *delim;
+	char *input;
+	char buffer[100];
+	char namefile[21];
+	int fd;
+	(void)list;
+	(void)env;
+	//char last_input[10];
+	int flag = 0;
 
-  while (final) {
-    while (final->tokens) {
-      if (final->tokens->type == 3)
-	  {
-        int rand = open("/dev/urandom", O_RDWR);
-        int j = 0;
-        int i = 0;
-        while (1) {
-          read(rand, buffer, 100);
-          buffer[99] = '\0';
-          while (buffer[i]) {
-            if (ft_isalnum(buffer[i])) {
-              namefile[j] = buffer[i];
-              j++;
-            }
-            i++;
-          }
-          if (j == 20)
-            break;
-        }
-        close(rand);
-        namefile[19] = '\0';
-        fd = open(namefile, O_RDWR | O_CREAT);
-        delim = ft_strdup(final->tokens->next->content);
-        	input = readline(">");
-        	if (!input || ft_strcmp(input, delim))
-         	 break;
-        	write(fd, input, ft_strlen(input));
+	while (final) 
+	{
+		while (final->tokens)
+		{
+			if (final->tokens->type == 3)
+			{
+				int rand = open("/dev/urandom", O_RDWR);
+				int j = 0;
+				int i = 0;
+				while (1)
+				{
+					read(rand, buffer, 100);
+					buffer[99] = '\0';
+					while (buffer[i])
+					{
+						if (ft_isalnum(buffer[i]))
+						{
+							namefile[j] = buffer[i];
+							j++;
+						}
+						i++;
+					}
+					if (j == 20)
+						break;
+				}
+				close(rand);
+				namefile[19] = '\0';
+				fd = open(namefile, O_RDWR | O_CREAT);
+				delim = ft_strdup(final->tokens->next->content);
+				while(1)
+				{
+					input = readline(">");
+					if (!input || !ft_strcmp(input, delim))
+				 		break;
+					write(fd, input, ft_strlen(input));
+					flag = 1;
+				}
+			}
+			final->tokens = final->tokens->next;
+			if (flag){
+				dup2(fd,final->fd_in);
+				close(fd);
+			}
+		}
+		final = final->next;
 	}
-      read(fd, last_input, 10) ;printf("%s\n", last_input);
-
-      final->tokens = final->tokens->next;
-    }
-    final = final->next;
-  }
 }
--- parsing/init_shell.c
+++ parsing/init_shell.c
@@ -6,7 +6,7 @@
 /*   By: asebrani <asebrani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 17:10:58 by cbajji            #+#    #+#             */
-/*   Updated: 2024/10/15 17:14:05 by asebrani         ###   ########.fr       */
+/*   Updated: 2024/10/15 19:52:34 by asebrani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -133,18 +133,18 @@ void display_prompt(t_list shell, char **env, env_vars *list_env)
         // }
         check_for_delimeter(list);
         lines = tokens_to_lines(list);
-        t_line *current = lines;
-        while (current)
-        {
-            t_node *curr = current->tokens;
-            printf("this is line:\n");
-            while (curr)
-            {
-                printf("this is token : %s\n", curr->content);
-                curr = curr->next;
-            }
-            current = current->next;
-        }
+        //t_line *current = lines;
+        // while (current)
+        // {
+        //     t_node *curr = current->tokens;
+        //     printf("this is line:\n");
+        //     while (curr)
+        //     {
+        //         printf("this is token : %s\n", curr->content);
+        //         curr = curr->next;
+        //     }
+        //     current = current->next;
+        // }
         last_command(list_env, lines);
         handle_heredoc(lines, list_env,env);
         handle_pipe(lines,env,list_env);
